b6a7e28456cd4faf475234944a13b1d932f8fbb2
marcelowebdesigner <marcelocamusso@hotmail.com>
2026-09-15 01:00:11 +0000

    actualizacion workflows colaborativos

---
+++ src/ExpColaborativos/Untitled.R
@@ -0,0 +1,16 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: R:percent
+#     text_representation:
+#       extension: .R
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.19.5
+#   kernelspec:
+#     display_name: R
+#     language: R
+#     name: ir
+# ---
+
+# %%
